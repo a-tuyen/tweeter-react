44827593639734f2ad3a9c00361612c30a7a968d
Amanda <atuyen26@gmail.com>
2021-07-07 23:40:07 -0700

    formatting

--- src/components/Nav.jsx
+++ src/components/Nav.jsx
@@ -2,5 +2,16 @@ import React from "react";
 
 export default function Nav () {
 
+  return (
+
+    <nav>
+      <span>
+        <span class="logo">tweeter</span>
+        <button type="button" id="form-toggle-button"><span><span class="write1">Write</span> a new tweet</span></button>
+      </span>
+    </nav>
+
+  )
+
 }
 

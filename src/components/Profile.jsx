08786f27315407b9508228f64a39a263af2c803b
Amanda <atuyen26@gmail.com>
2021-07-09 23:41:20 -0700

    add in profile html

--- src/components/Profile.jsx
+++ src/components/Profile.jsx
@@ -3,6 +3,16 @@ import React from 'react';
 export default function Profile () {
 
   return (
+    
+    <header>
+      <div>
+        <img src="/images/profile-hex.png"/> 
+      </div>
+      <div>
+        <h2>Amanda Tuyen</h2>
+      </div>
+  </header>
+  
 
   )
 }
